0c1f259272e901fad760b2d871e075ce0bb21c1b
IsaGher <sg_0300@hotmail.com>
2021-06-16 10:07:22 -0600

    metodo de taylor para n orden

--- prueba.py
+++ prueba.py
@@ -1,4 +1,8 @@
 from euler import Euler
+from taylor import Taylor
 
-pru = Euler("y+2*x*exp(2*x)",0,1,3,3)
-print(pru.eulerAtras())
+#pru = Euler("y+2*x*exp(2*x)",0,1,3,3)
+#print(pru.eulerAtras())
+
+prue = Taylor("y-x",1,3,5,5,3)
+print(prue.taylor())
